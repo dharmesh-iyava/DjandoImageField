b1b242bf10c9fca61f7b781f6c3dae6f5099c28b
dharmesh-iyava <60869379+dharmesh-iyava@users.noreply.github.com>
2020-06-19 20:02:06 +0530

    Delete views.py

--- views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.core.mail import send_mail
-from .models import *
-
-
-def home(request):
-    exa = Example.objects.all()
-
-    return render(request, 'home.html', {'exa': exa})
